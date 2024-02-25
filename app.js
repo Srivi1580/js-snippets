66c67e1c90fe7a7ae4f746b65f7b4f29161cc266
srivi1580 <dsrivasavi1580@gmail.com>
2024-02-25 16:27:58 +0000

    basic todo list

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-const a = 25;
-console.log(a);
+const allvalues=[];
+function add(){
+    
+    const inputvalue= document.getElementById('in').value
+    allvalues.push(inputvalue)
+   // let list=`<li> ${inputvalue} </li>`
+    
+    console.log(inputvalue);
+    looplist()
+
+}
+function looplist(){
+list =` `;
+for(i=0; i<allvalues.length;i++){
+list=list+ `<li>${allvalues[i]} </li>`
+}
+document.getElementById('lis').innerHTML = list
+}
